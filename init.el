ea9ba2f7523759270381d8cc0c5fbe316fd650d5
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-27 22:24:36 -0500

    Simplify

--- init.el
+++ init.el
@@ -11,93 +11,26 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(setq evil-want-keybinding nil)
-(menu-bar-mode -1)
 
 (straight-use-package 'use-package)
-
-(setq vterm-always-compile-module t)
 
 (use-package evil
  :straight t
  :ensure t
+ :init
+ (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+ (setq evil-want-keybinding nil)
  :config
  (evil-mode 1))
 
 (use-package evil-collection
- :straight (:host github :repo "mjlbach/evil-collection"
-            :no-byte-compile t
+ :after evil
+ :ensure t
+ :straight (:host github :repo "mjlbach/evil-collection" :no-byte-compile t
             :branch "feature/additional_libvterm_evil_bindings")
  :config
- (evil-collection-init)
- )
+ (evil-collection-init))
 
+(setq vterm-always-compile-module t)
 (use-package vterm
- :straight t
- :config
- )
-
-(use-package selectrum
- :straight t
- :config 
- (selectrum-mode +1))
-
-(use-package nord-theme
-  :straight t
-  :config
-  (load-theme 'nord t)
-  )
-  
-(use-package general
-  :straight t
-  :ensure t
-  )
-
-(use-package which-key
-  :straight t
-  :ensure t
-  :config
-  (which-key-mode)
-  )
-
-;; Example configuration for Consult
-(use-package consult
-  :straight t
-  :init
-  (setq register-preview-delay 0
-        register-preview-function #'consult-register-preview)
-  (advice-add #'register-preview :around
-              (lambda (fun buffer &optional show-empty)
-                (let ((register-alist (seq-sort #'car-less-than-car register-alist)))
-                  (funcall fun buffer show-empty))
-                (when-let (win (get-buffer-window buffer))
-                  (with-selected-window win
-                    (setq-local mode-line-format nil)
-                    (setq-local window-min-height 1)
-                    (fit-window-to-buffer)))))
-  :config
-
-  (setq consult-narrow-key "<") ;; (kbd "C-+")
-  ;;(autoload 'projectile-project-root "projectile")
-  ;;(setq consult-project-root-function #'projectile-project-root))
-  )
-
-(define-key selectrum-minibuffer-map (kbd "<escape>") #'abort-recursive-edit)
-
-(general-create-definer my-leader-def
-  ;; :prefix my-leader
-  :prefix "SPC")
-
-(defun my-fdfind (&optional dir)
-  (interactive "P")
-  (let ((consult-find-command '("fd" "--color=never" "--full-path")))
-    (consult-find dir)))
-
-(my-leader-def
-  :keymaps 'normal
-  ;; bind "SPC a"
-  "a" 'org-agenda
-  "b" 'counsel-bookmark
-  "c" 'org-capture
-  "f" 'my-fdfind
-  "s" 'consult-ripgrep)
+ :straight t)
